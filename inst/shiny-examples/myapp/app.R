454aada5790a42126f289f2ec142251a181982ff
mgerault <67689583+mgerault@users.noreply.github.com>
2022-03-30 13:51:32 +0200

    Update app.R

--- inst/shiny-examples/myapp/app.R
+++ inst/shiny-examples/myapp/app.R
@@ -938,8 +938,6 @@ server <- function(input, output, session){
         paste0("ProteinGroup_dia_", Sys.Date(), ".", input$format_pg)
       },
       content = function(file){
-        print(input$format_pg)
-        print(file)
         if(input$format_pg == "xlsx"){
           openxlsx::write.xlsx(pg_ev$x, file, rowNames = FALSE)
         }
